d1463bf919709a91e2d5c19240109ccb805130b2
RyanGBC <ryangabrj@gmail.com>
2025-03-11 09:40:09 -0300

    feat: Projeto Devops

---
+++ ApiPiaget/Models/Aluno.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiPiaget.Models
+{
+    public class Aluno
+    {
+        public int id { get; set; }
+        public string? Nome { get; set; }
+        public string? RGM { get; set; }
+        public decimal Nota { get; set; }
+
+
+
+
+    }
+}
